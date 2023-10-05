ff56c339e57f19c6dc1f2b15c69a52772d3b1b68
zeptozor <zeptozor@gmail.com>
2023-10-05 16:31:10 +0600

    лилоил

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import Head from "@/components/Home/Head";
+import Regions from "@/components/Home/Regions";
+import Nav from "@/components/Nav";
+
 export default function Home() {
   return (
-    <div>
-    </div>
+    <>
+      <Nav />
+      <Head />
+      <Regions />
+    </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,30 +16,46 @@ const config: Config = {
       },
       colors: {
         'orange': '#F90',
-        'gray': '848D9F',
+        'gray': '#848D9F',
         'bg': '#F6F7F9',
         'green': '#33C476',
-        'red': '#C43333'
+        'red': '#C43333',
+        'blue': '#0075FF'
       },
       boxShadow: {
         'small': '0px 0px 31px 0px rgba(0, 0, 0, 0.05)'
       },
       fontSize: {
+        48: '48px',
         15: '15px',
         26: '26px',
         28: '28px',
         32: '32px',
         36: '36px'
-      }
+      },
+      padding: {
+        80: '80px',
+        70: '70px',
+        40: '40px',
+        20: '20px'
+      },
+      margin: {
+        80: '80px',
+        70: '70px',
+        40: '40px',
+        20: '20px'
+      },
     },
     borderRadius: {
       DEFAULT: '10px'
     },
     lineHeight: {
+      100: '100%',
       120: '120%',
       150: '150%'
     },
     gap: {
+      40: '40px',
       20: '20px',
       10: '10px',
       5: '5px'
@@ -47,13 +63,17 @@ const config: Config = {
   },
   plugins: [
     plugin(({ matchUtilities }) => {
-      matchUtilities({
-        image: (value) => ({
-              backgroundImage: `url('../public/images/${value}')`,
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: '100%'
-          })
-      })
+      // matchUtilities({
+      //   image: (value) => {
+          
+      //     console.log(`url('../public/images/regions/${value}')`)
+      //     return {
+      //       backgroundImage: `url('../public/images/regions/${value}')`,
+      //       backgroundRepeat: 'no-repeat',
+      //       backgroundSize: '100%'
+      //     }
+      //   }
+      // })
     })
   ],
 }
